5393ae5fb65769640f1c3c17b0d16a1119042bb6
Devendar Singh Rawat <144051384+devendar007@users.noreply.github.com>
2026-04-08 01:32:03 +0530

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        if(fast==null) return false;
+        while(fast!=null&&fast.next!=null){
+     slow=slow.next;
+     fast=fast.next.next;
+     if(slow==fast) return true;
+        }
+        return false;
+    }
+}
